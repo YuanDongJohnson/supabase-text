6f87df3c9b6c10483558e249a5fe453fe1a8b038
YuanDongJohnson <163552945+YuanDongJohnson@users.noreply.github.com>
2024-10-06 04:56:47 +0800

    Update AuthForm.tsx

--- components/auth/AuthForm.tsx
+++ components/auth/AuthForm.tsx
@@ -1,10 +1,9 @@
-"use client"; // 确保这行代码在文件的最顶部
-
-import React, { useState, useEffect } from 'react';
+import React from "react";
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
 import SubmitButton from "@/components/Common/Submit-Button";
 import Link from "next/link";
+import { headers } from "next/headers";
 import { Divider } from "@nextui-org/react";
 import SocialAuth from "@/components/auth/SocialAuth";
 import EmailInput from "@/components/auth/EmailInput";
@@ -13,98 +12,65 @@ import MessageCard from "@/components/Common/MessageCard";
 import ErrorCard from "@/components/Common/ErrorCard";
 import { Checkbox } from '@nextui-org/react';
 
+
 interface AuthForm {
   method: "login" | "signup";
   searchParams: Record<string, string | string[] | undefined>;
 }
 
-const AuthForm = ({ method, searchParams }: AuthForm) => {
+const AuthForm = async ({ method, searchParams }: AuthForm) => {
   const supabase = createClient();
-  const [user, setUser] = useState(null);
-  const [rememberMe, setRememberMe] = useState(false);
-  const [username, setUsername] = useState('');
 
-  useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        const {
-          data: { user },
-        } = await supabase.auth.getUser();
-        setUser(user);
-      } catch (error) {
-        console.error("Failed to fetch user", error);
-      }
-    };
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
 
-    fetchUser();
-  }, []);
-
-  useEffect(() => {
-    const storedUsername = localStorage.getItem('username');
-    if (storedUsername) {
-      setUsername(storedUsername);
-      setRememberMe(true);
-    }
-  }, []);
-
-  const handleRememberMeChange = (checked) => {
-    setRememberMe(checked);
-    if (checked) {
-      localStorage.setItem('username', username);
-    } else {
-      localStorage.removeItem('username');
-    }
-  };
+  if (user) {
+    return redirect(`/profile`);
+  }
 
   const signIn = async (formData: FormData) => {
+    "use server";
+
     const email = formData.get("email") as string;
     const password = formData.get("password") as string;
     const supabase = createClient();
 
-    try {
-      const { error } = await supabase.auth.signInWithPassword({
-        email,
-        password,
-      });
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
 
-      if (error) {
-        return redirect(`/auth/login?error=${error.message}`);
-      }
-
-      return redirect(searchParams.next ? `/${searchParams.next}` : "/");
-    } catch (error) {
-      console.error("Sign in error:", error);
+    if (error) {
       return redirect(`/auth/login?error=${error.message}`);
     }
-  };
 
+    return redirect(searchParams.next ? `/${searchParams.next}` : "/");
+  };
   const signUp = async (formData: FormData) => {
+    "use server";
+
+    const origin = headers().get("origin");
     const email = formData.get("email") as string;
     const password = formData.get("password") as string;
     const supabase = createClient();
 
-    try {
-      const { data, error } = await supabase.auth.signUp({
-        email,
-        password,
-        options: {
-          emailRedirectTo: `${origin}/auth/callback`,
-        },
-      });
+    const { data, error } = await supabase.auth.signUp({
+      email,
+      password,
+      options: {
+        emailRedirectTo: `${origin}/auth/callback`,
+      },
+    });
 
-      if (error) {
-        return redirect(`/auth/signup?error=${error.message}`);
-      }
-
-      if (data) {
-        return redirect("/auth/signup?message=Check email to continue sign in process");
-      }
-    } catch (error) {
-      console.error("Sign up error:", error);
+    if (error) {
       return redirect(`/auth/signup?error=${error.message}`);
     }
-  };
 
+    return redirect(
+      "/auth/signup?message=Check email to continue sign in process"
+    );
+  };
   return (
     <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-4">
       <SocialAuth />
@@ -112,25 +78,20 @@ const AuthForm = ({ method, searchParams }: AuthForm) => {
       <form className="animate-in flex flex-col w-full justify-center gap-4 text-foreground">
         {searchParams.message && <MessageCard message={searchParams.message} />}
         {searchParams.error && <ErrorCard error={searchParams.error} />}
-        <EmailInput value={username} onChange={(e) => setUsername(e.target.value)} />
+        <EmailInput />
         <div className="flex flex-col gap-2">
-          <PasswordInput method={method} />
-          <div className="flex items-center justify-between">
-            <Checkbox
-              name="remember"
-              checked={rememberMe}
-              onChange={handleRememberMeChange}
-            >
-              Remember Me
-            </Checkbox>
-            <Link
-              className="text-blue-500"
-              href={"/auth/reset-password"}
-            >
-              Forgot Password?
-            </Link>
-          </div>
-        </div>
+  <PasswordInput method={method} />
+  <div className="flex items-center justify-between">
+    <Checkbox name="remember">Remember Me</Checkbox>
+    <Link
+      className="text-blue-500"
+      href={"/auth/reset-password"}
+    >
+      Forgot Password?
+    </Link>
+  </div>
+</div>
+
         <SubmitButton
           formAction={method == "login" ? signIn : signUp}
           loadingText={method == "login" ? "Signing In..." : "Signing Up..."}
