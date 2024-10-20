d787714d8dc6ca797aa1c632d7544d7649ae3de4
YuanDongJohnson <163552945+YuanDongJohnson@users.noreply.github.com>
2024-10-20 10:56:51 +0800

    Update AuthButton.tsx

--- components/Layout/AuthButton.tsx
+++ components/Layout/AuthButton.tsx
@@ -1,47 +1,22 @@
-import { Button, Dialog, DialogOverlay, DialogContent, useDisclosure } from "@nextui-org/react";
+import { createClient } from "@/utils/supabase/server";
+import { Button } from "@nextui-org/react";
 import Link from "next/link";
 import { IoPersonCircleOutline } from "react-icons/io5";
 
-export default function AuthButton() {
-  const { isOpen, onOpen, onClose } = useDisclosure();
+export default async function AuthButton() {
   const supabase = createClient();
-  const [user, setUser] = useState(null);
 
-  // 在组件加载时检查用户是否已经登录
-  useEffect(() => {
-    const checkUser = async () => {
-      const { data: { user } } = await supabase.auth.getUser();
-      setUser(user);
-    };
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
 
-    checkUser();
-  }, [supabase]);
-
-  return (
-    <>
-      {user ? (
-        <Button as={Link} href="/profile" variant="bordered" startIcon={<IoPersonCircleOutline size={24} />}>
-          Profile
-        </Button>
-      ) : (
-        <Button onClick={onOpen} variant="bordered" startIcon={<IoPersonCircleOutline size={24} />}>
-          Login
-        </Button>
-      )}
-
-      <DialogOverlay isOpen={isOpen} onDismiss={onClose}>
-        <DialogContent>
-          <Button
-            as={Link}
-            href="/auth/login"
-            variant="bordered"
-            onClick={onClose}
-            autoFocus
-          >
-            Login
-          </Button>
-        </DialogContent>
-      </DialogOverlay>
-    </>
+  return user ? (
+    <Button href="/profile" variant="bordered" as={Link} isIconOnly>
+      <IoPersonCircleOutline size={24} />
+    </Button>
+  ) : (
+    <Button href="/auth/login" variant="bordered" as={Link}>
+      Login
+    </Button>
   );
 }
